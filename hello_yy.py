12aef4d81a816ea7e418e2f172aa3c7da1743c6a
chuanwhuang <1393076811@qq.com>
2020-06-15 20:43:05 +0800

    minor change

--- hello_yy.py
+++ hello_yy.py
@@ -5,6 +5,8 @@ import time
 def hello_func(n, t):
 	for i in range(n):
 		print('sleep in %d' % (i) )
-		time.sleep(t)
+		#time.sleep(t)
 raise RuntimeError('HelloError!')
 hello_func(1,5)
+a = 10 / 0
+print(a)
